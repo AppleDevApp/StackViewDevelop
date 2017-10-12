f630cbfb59a48650ad05da17aa192cd4a1581620
appledevapp <appledevapp@icloud.com>
2017-10-12 22:26:17 +0800

    develop view using stackview
    use photos

---
+++ StackViewDevelop/StackViewDevelop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackViewDevelop
+//
+//  Created by AppleDevApp on 12/10/17.
+//  Copyright © 2017年 AppleDevApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
